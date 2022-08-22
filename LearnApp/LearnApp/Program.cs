812aee3704f6448d272e00ebc6a1fb31f57e29b7
AlfredCooper <mxcooper71@gmail.com>
2022-08-22 21:44:09 +0300

    First lessons

--- LearnApp/LearnApp/Program.cs
+++ LearnApp/LearnApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace LearnApp
 {
@@ -10,8 +11,48 @@ namespace LearnApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
-            Console.WriteLine("Dimas pidaras!");
+            //Console.WriteLine("Вводим строку: ");
+            //string str = Console.ReadLine();
+
+            //Console.WriteLine("Выводим строку: ");
+            //Console.WriteLine(str);
+
+
+            //=================================================
+
+            //string str = "5.9";
+
+            //NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            //{
+            //    NumberDecimalSeparator = ".",
+            //};
+
+            //double a = double.Parse(str, numberFormatInfo);
+            //Console.WriteLine(a);
+
+            //=================================================
+
+            //string str = "5";
+
+            //try
+            //{
+            //    double a = double.Parse(str);
+            //    Console.WriteLine("Успешная конвертация");
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("Ошибка при конвертации");
+            //}
+
+            //=================================================
+
+            //string str = "4";
+
+            //int b;
+
+            //bool result = int.TryParse(str, out b);
+
         }
     }
 }
