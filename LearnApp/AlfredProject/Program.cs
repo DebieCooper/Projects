3506cb30a3dcaae2f1bacbb65f5d9605b4916fe7
AlfredCooper <mxcooper71@gmail.com>
2022-09-11 20:19:31 +0300

    Add new project

---
+++ LearnApp/AlfredProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AlfredProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            a = ++a * a;
+            Console.WriteLine(a);
+        }
+    }
+}
