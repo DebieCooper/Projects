3e34a316f1e61a30a2bdd1ba94f6d9ee4924b24c
AlfredCooper <mxcooper71@gmail.com>
2022-09-15 20:38:10 +0300

    Triangle

--- LearnApp/AlfredProject/Program.cs
+++ LearnApp/AlfredProject/Program.cs
@@ -4,9 +4,54 @@
     {
         static void Main(string[] args)
         {
-            int a = 1;
-            a = ++a * a;
-            Console.WriteLine(a);
+            //int a = 1;          
+            //a = ++a * a;
+            //Console.WriteLine(a);
+
+
+            //Console.Write("Введите высоту треугольника: ");
+            //int height = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < height; i++)
+            //{
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //======================================================
+
+            //Console.Write("Введите высоту треугольника: ");
+
+            //int height = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < height; i++)
+            //{
+            //    for (int j = height; j > i; j--)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            Console.Write("Введите высоту треугольника: ");
+
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i ; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
